7d866339616262c2a0b080b4496b4db09b56ddbe
Jakub <jakub180720@gmail.com>
2024-12-21 13:04:05 +0100

    feat - page recommended movies and movie tiles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
@@ -8,12 +8,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopbarComponent } from './components/topbar/topbar.component';
 import { HomeComponent } from './pages/home/home.component';
+import { RecommendedMoviesComponent } from './pages/recommended-movies/recommended-movies.component';
+import { MovieTileComponent } from './components/movie-tile/movie-tile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopbarComponent,
-    HomeComponent
+    HomeComponent,
+    RecommendedMoviesComponent,
+    MovieTileComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,6 +27,7 @@ import { HomeComponent } from './pages/home/home.component';
     MatIconModule
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/movie-tile/movie-tile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-tile',
+  templateUrl: './movie-tile.component.html',
+  styleUrl: './movie-tile.component.scss'
+})
+export class MovieTileComponent {
+
+}
